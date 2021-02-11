2c55d2020a19ed28e739cdd87c531689f5cbb6d1
Jose Carlos Herrera <jcherrera98@gmail.com>
2021-02-11 00:39:15 -0600

    avanzando

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
-const MaterialComponents = [MatIconModule, MatToolbarModule, MatCardModule];
+const MaterialComponents = [
+  MatIconModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatGridListModule,
+  MatButtonModule,
+];
 
 @NgModule({
   declarations: [],
