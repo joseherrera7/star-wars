54b8069d861085dd3334a461a4bbe859f4cd5f6e
Jose Carlos Herrera <jcherrera98@gmail.com>
2021-05-03 23:35:32 -0600

    agregando ultimos cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: 'http://ec2co-ecsel-1wag45dzl2q64-519010488.us-east-2.elb.amazonaws.com:2021'
+  server: 'https://luli-server-service-tecsoqd6vq-uc.a.run.app'
 };
